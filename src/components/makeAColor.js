c7f52ec03a9cab27b4fd11998b07141c872cd964
Grant Reighard <grant.reighard@gmail.com>
2020-11-22 16:09:21 -0500

    Swaps name and hex positions on random box.

--- src/components/makeAColor.js
+++ src/components/makeAColor.js
@@ -25,7 +25,7 @@ export default function MakeAColor(props) {
     return (
         <div className="make-a-color">
             <h1>Random unnamed color<br/>(according to CSS)</h1>
-            <div className="color-box" style={{backgroundColor: randomColor, color: invert(randomColor, true)}}><h1>{randomColor}</h1><h1>{nearest(randomColor).name}</h1></div>
+            <div className="color-box" style={{backgroundColor: randomColor, color: invert(randomColor, true)}}><h1>{nearest(randomColor).name}</h1><h1>{randomColor}</h1></div>
             <button onClick={makeAnUnnamedColor}>Generate random color</button>
         </div>
         
